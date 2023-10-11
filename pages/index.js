94bd672af4ca762c2009de473c98781c3aef616d
Akshay Patel <ap7839426@gmail.com>
2023-10-11 12:37:42 +0530

    added NFT cards section to the webpage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 "use client";
 import {useState,useEffect,useRef} from 'react';
-import {Banner,CreaterCard} from '../components';
+import {Banner,CreaterCard,NFTCard} from '../components';
 import images from '../assets';
 import { makeId } from '@/utils/makeId';
 import Image from 'next/image';
@@ -76,7 +76,28 @@ const Home = () => {
         </div>
         </div>
       </div>
-
+      <div className='mt-10'>
+          <div className='flexBetween mx-4 xs:mx-0 minlg:mx-8 sm:flex-col sm:items-start'>
+          <h1 className='flex-1 before:font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold sm:mb-4'>Best NFTS</h1>
+          <div>Search</div>
+          </div>
+          <div className='mt-3 w-full flex flex-wrap justify-start md:justify-center'>
+            {[1,2,3,4,5,6,7,8,9,10].map((i)=>(
+              <NFTCard
+                key={`nft-${i}`}
+                nft={{
+                  i:i,
+                  name:`Nifty NFT ${i}`,
+                  price:(10-i*0.534).toFixed(2),
+                  seller:`creator${i}`,
+                  owner: `owner${i}`,
+                  description: 'cool nft on sale',
+                }}
+              />
+            ))
+            }
+          </div>
+      </div>
     </div>
   </div>
 );
